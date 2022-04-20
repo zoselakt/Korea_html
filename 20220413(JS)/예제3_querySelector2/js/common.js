c2c33b3d0eea7f0281238043a094e0d99cee27ea
zoselakt <kd66@naver.com>
2022-04-20 16:45:28 +0900

    Revert "first JavaScript"
    This reverts commit 9c5058d9e41f9fb44d8cefa698e5bb5823a802eb.

--- 20220413(JS)/예제3_querySelector2/js/common.js
+++
@@ -1,8 +0,0 @@
-'use sttict'
-
-function innerTxt(){
-    let inputNum = document.querySelector("#input-text").value;
-    document.querySelector(".content > h2 > p").innerHTML = inputNum;
-    // document.querySelector(".content > p").innerHTML = "hello QuerySelector";
-    // document.querySelector("inputcon");
-}
