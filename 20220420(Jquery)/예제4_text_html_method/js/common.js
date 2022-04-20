489fbf482b9880a2f563db56ea99f98b802179b5
zoselakt <95839182+zoselakt@users.noreply.github.com>
2022-04-20 17:26:01 +0900

    Add files via upload

---
+++ 20220420(Jquery)/예제4_text_html_method/js/common.js
@@ -0,0 +1,26 @@
+'use strict'
+$(function(){
+    /*
+    let textNum = '';
+    textNum = $("p#first");
+    textNum.text('hi jquery');
+
+    //특정요소의 텍스트를 다른요소에 삽입하는 예
+    // 방법1
+    // $("p#second").text();
+    console.log($("p#second").text());
+    $("p#first").text($("p#second").text())
+
+    // 방법2
+    // a type
+    let elem = $("p#first");
+    let elemInputText = $("p#second").text();
+    elem.text(elemInputText);
+*/
+    // b type
+    let elem = $("p#first");
+    let elemInput = $("p#second");
+    elem.text(elemInput.text());
+
+    let numHtml = "";
+});
